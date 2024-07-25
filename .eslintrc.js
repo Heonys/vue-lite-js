126ed69957168d4a9d289b84e9248008511e3987
Jiheon Kim <siwmua0@naver.com>
2024-07-25 18:28:49 +0900

    :tada: Initalize project vuelite with Gitmoji

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      plugins: ["@typescript-eslint"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-namespace": "off",
+  },
+};
